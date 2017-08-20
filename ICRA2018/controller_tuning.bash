f60f895e3fa46a70eee604d627d9b9bb025afdab
sytang <sarahyifang@gmail.com>
2017-08-20 18:53:23 -0400

    Tuned 3d controller for pointload feedback

--- ICRA2018/controller_tuning.bash
+++ ICRA2018/controller_tuning.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROBOT="QuadrotorDopey"
+ROBOT="QuadrotorHappy"
 
 read -p "Press [Enter] to start motors"
 echo "Enable motors..."
@@ -12,10 +12,15 @@ echo "Takeoff..."
 rosservice call /$ROBOT/mav_services/takeoff
 sleep 1
 
-read -p "Press [Enter] to go to initial position"
-echo "Going to position..."
-rosservice call /$ROBOT/mav_services/goTo '{goal: [0.0, 0.0, 1.0, 0.0]}'
+read -p "Press [Enter] to hover in place"
+echo "Hovering..."
+rosservice call /$ROBOT/mav_services/hover
 sleep 1
+
+#read -p "Press [Enter] to go to initial position"
+#echo "Going to position..."
+#rosservice call /$ROBOT/mav_services/goTo '{goal: [0.0, 0.0, 1.5, 0.0]}'
+#sleep 1
 
 read -p "Press [Enter] to switch to payload tracking"
 echo "Switching to payload control..."
@@ -27,3 +32,7 @@ echo "Moving right..."
 rosservice call /$ROBOT/mav_services/goToRelative '{goal: [1.0, 0.0, 0.0, 0.0]}'
 sleep 1
 
+read -p "Press [Enter] to switch to quadrotor tracking"
+echo "Switching to quadrotor control..."
+rosservice call /$ROBOT/trackers_manager/track_payload '{data: false}'
+sleep 1
