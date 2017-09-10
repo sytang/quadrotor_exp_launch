601606344cb7d55b013d1e6f576cd31d0a6f2ad9
Valentin Wueest <vwueest@seas.upenn.edu>
2017-09-09 21:31:38 -0400

    ICRA experiments final

--- ICRA2018/cone_experiment.bash
+++ ICRA2018/cone_experiment.bash
@@ -14,7 +14,7 @@ sleep 1
 
 read -p "Press [Enter] to go to initial position"
 echo "Going to position..."
-rosservice call /quadrotor/mav_services/goTo '{goal: [6.3, -0.39, 1.578, 0.0]}'
+rosservice call /quadrotor/mav_services/goTo '{goal: [6.3, 0.0, 1.178, 0.0]}'
 sleep 1
 
 read -p "Press [Enter] to switch to payload tracking"
@@ -24,13 +24,8 @@ sleep 15
 
 # read -p "Press [Enter] to load trajectory waypoints"
 echo "Loading waypoints..."
-<<<<<<< HEAD
-rosservice call /QuadrotorHappy/mav_services/loadTimedWaypoints "{x: [6.3, 5.1, 4.66, 4.22, 3.78, 3.34, 2.9, 2.46, 1.67], y:[-0.29, 0.29, 0.0, -0.29, 0.0, 0.29, 0.0, -0.29, 0.29], z:[0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5], yaw:[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], times: [5.33126626609, 6.33126626609, 7.33126626609, 8.33126626609, 9.33126626609, 10.3312662661, 11.3312662661, 15.2514703424], cost: 6}" 
-sleep 1
-=======
-rosservice call /quadrotor/mav_services/loadTimedWaypoints "{x: [6.3, 4.66, 4.22, 3.78, 3.34, 2.9, 2.46, 3.78, 2.99], y:[-0.39, 0.39, 0.0, -0.39, 0.0, 0.39, 0.0, -0.39, 0.39], z:[1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1], yaw:[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], times: [8.58733951815, 10.0873395182, 11.5873395182, 13.0873395182, 14.5873395182, 16.0873395182, 17.5873395182, 19.0873395182, 20.5873395182, 27.2484205115], cost: 6}" 
+rosservice call /quadrotor/mav_services/loadTimedWaypoints "{x: [6.3, 5.1, 4.66, 4.22, 3.78, 3.34, 2.9, 2.46, 1.67], y:[0.0, 0.49, 0.0, -0.49, 0.0, 0.49, 0.0, -0.49, 0.49], z:[0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7], yaw:[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], times: [2.3331360869, 2.7831360869, 3.2331360869, 3.6831360869, 4.1331360869, 4.5831360869, 5.0331360869, 7.29892071921], cost: 6}" 
 sleep 2
->>>>>>> 2d84f74b3e8fab9c434ecdb8de525f5c120292ae
 
 # read -p "Press [Enter] to follow trajectory waypoints"
 echo "Following waypoints..."
